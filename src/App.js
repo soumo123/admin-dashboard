e490a772108d94bc52406db877fda2a3cf95ffdc
Soummya Biswas <soummyabiswas999@gmail.com>
2024-08-26 00:36:22 +0530

    changes in app.js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,111 @@
+// import React, { useEffect } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+// import CreateProduct from './components/CreateProduct';
+// import Sidenav from './components/Sidenav';
+// import { Routes, Route } from "react-router-dom";
+// import AllProduct from './components/AllProduct';
+// import AllUsers from './components/AllUsers';
+// import Tags from './components/Tags';
+// import { useParams } from 'react-router-dom'
+// import Settings from './components/Settings';
+// import { useLocation } from 'react-router-dom';
+// import Dashboard from './components/Dashboard';
+// import EditProduct from './components/EditProduct';
+// import ViewProduct from './components/ViewProduct';
+// import Orders from './components/Orders';
+// import Vendor from './components/Vendor';
+// import AddVendorProducts from './custom/AddVendorProducts';
+// import UpdateStock from './components/UpdateStock';
+// import Transaction from './components/Transaction';
+// import Taxes from './components/Taxes';
+// import Notifications from './components/Notifications';
+// import Expiredproducts from './components/Expiredproducts';
+// import Requestedorders from './components/Requestedorders';
+
+
+
+// function App() {
+//   const location = useLocation();
+
+//   const queryParams = new URLSearchParams(location.search); // Parse query parameters
+//   const type = queryParams.get('type');
+//   const adminId = queryParams.get('adminId');
+//   const shop_id = queryParams.get('shop_id');
+
+
+//   useEffect(() => {
+//     if (adminId && shop_id && type) {
+
+//       localStorage.setItem("adminId", window.atob(adminId))
+//       localStorage.setItem("shop_id", window.atob(shop_id))
+//       localStorage.setItem("type", window.atob(type))
+//     }
+
+//   }, [adminId,
+//     shop_id,
+//     type])
+//   console.log("type, adminId, shop_id ---App.js", type, adminId, shop_id)
+
+//   return (
+//     <>
+
+
+//       <div id="app" style={({ height: "100vh" })}>
+//         <div className="admin-box">
+//           <div className="admin-sidebar">
+//             <Sidenav />
+//           </div>
+
+//           <div className="admin-content">
+//             <main>
+
+//               <div style={{}}>
+
+//                 <Routes>
+//                   <Route exact={true} path={`/`} element={<Dashboard />} />
+//                   {/* <Route exact={true} path={`/create`} element={<CreateProduct />} /> */}
+//                   <Route exact={true} path="/allproducts" element={<AllProduct />} />
+//                   <Route exact={true} path="/updateproduct/:id" element={<EditProduct />} />
+//                   <Route exact={true} path="/vewProduct/:id" element={<ViewProduct />} />
+//                   <Route exact={true} path="/allusers" element={<AllUsers />} />
+//                   <Route exact={true} path="/tags" element={<Tags />} />
+//                   <Route exact={true} path="/settings" element={<Settings />} />
+//                   <Route exact={true} path="/manage-order" element={<Orders />} />
+//                   <Route exact={true} path="/vendors" element={<Vendor />} />
+
+//                   <Route exact={true} path="/addVendorProduct/:agentId/:vendorId" element={<AddVendorProducts />} />
+//                   <Route exact={true} path="/stocks" element={<UpdateStock />} />
+//                   <Route exact={true} path="/transaction" element={<Transaction />} />
+//                   <Route exact={true} path="/tax" element={<Taxes />} />
+
+//                   <Route exact={true} path="/notifications" element={<Notifications />} />
+//                   <Route exact={true} path="/expired" element={<Expiredproducts />} />
+//                   <Route exact={true} path="/requests" element={<Requestedorders />} />
+
+
+//                 </Routes>
+//               </div>
+//             </main>
+//           </div>
+//         </div>
+
+
+
+
+
+//       </div>
+//     </>
+//   );
+// }
+
+// export default App;
 import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CreateProduct from './components/CreateProduct';
 import Sidenav from './components/Sidenav';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route,Navigate  } from "react-router-dom";
 import AllProduct from './components/AllProduct';
 import AllUsers from './components/AllUsers';
 import Tags from './components/Tags';
@@ -22,6 +124,7 @@ import Taxes from './components/Taxes';
 import Notifications from './components/Notifications';
 import Expiredproducts from './components/Expiredproducts';
 import Requestedorders from './components/Requestedorders';
+import Authentication from './components/Authentication';
 
 
 
@@ -32,6 +135,8 @@ function App() {
   const type = queryParams.get('type');
   const adminId = queryParams.get('adminId');
   const shop_id = queryParams.get('shop_id');
+
+
 
 
   useEffect(() => {
@@ -45,7 +150,12 @@ function App() {
   }, [adminId,
     shop_id,
     type])
-  console.log("type, adminId, shop_id ---App.js", type, adminId, shop_id)
+
+    const exactadminId = localStorage.getItem("adminId")
+    const exactShopId = localStorage.getItem("shop_id")
+    const exactType = localStorage.getItem("type")
+    
+    console.log("adminIdadminIdadminIdadminId",exactadminId)
 
   return (
     <>
@@ -54,47 +164,65 @@ function App() {
       <div id="app" style={({ height: "100vh" })}>
         <div className="admin-box">
           <div className="admin-sidebar">
-            <Sidenav />
+            {
+               exactadminId==="" ? (
+                ""
+               ):(
+                <Sidenav />
+               )
+            }
+          
           </div>
 
           <div className="admin-content">
             <main>
-
               <div style={{}}>
+                {
+                  exactadminId==="" ? (
+                    <Routes>
+                      <Route exact={true} path="/" element={<Authentication />} />
+                      <Route path="*" element={<Navigate to="/" />} /> {/* Redirect to / */}
+                    </Routes>
+                  ) : (
+                    <Routes>
+                      <Route exact={true} path={`/`} element={<Dashboard />} />
+                      {/* <Route exact={true} path={`/create`} element={<CreateProduct />} /> */}
+                      <Route exact={true} path="/allproducts" element={<AllProduct />} />
+                      <Route exact={true} path="/updateproduct/:id" element={<EditProduct />} />
+                      <Route exact={true} path="/vewProduct/:id" element={<ViewProduct />} />
+                      <Route exact={true} path="/allusers" element={<AllUsers />} />
+                      <Route exact={true} path="/tags" element={<Tags />} />
+                      <Route exact={true} path="/settings" element={<Settings />} />
+                      <Route exact={true} path="/manage-order" element={<Orders />} />
+                      <Route exact={true} path="/vendors" element={<Vendor />} />
 
-                <Routes>
-                  <Route exact={true} path={`/`} element={<Dashboard />} />
-                  {/* <Route exact={true} path={`/create`} element={<CreateProduct />} /> */}
-                  <Route exact={true} path="/allproducts" element={<AllProduct />} />
-                  <Route exact={true} path="/updateproduct/:id" element={<EditProduct />} />
-                  <Route exact={true} path="/vewProduct/:id" element={<ViewProduct />} />
-                  <Route exact={true} path="/allusers" element={<AllUsers />} />
-                  <Route exact={true} path="/tags" element={<Tags />} />
-                  <Route exact={true} path="/settings" element={<Settings />} />
-                  <Route exact={true} path="/manage-order" element={<Orders />} />
-                  <Route exact={true} path="/vendors" element={<Vendor />} />
+                      <Route exact={true} path="/addVendorProduct/:agentId/:vendorId" element={<AddVendorProducts />} />
+                      <Route exact={true} path="/stocks" element={<UpdateStock />} />
+                      <Route exact={true} path="/transaction" element={<Transaction />} />
+                      <Route exact={true} path="/tax" element={<Taxes />} />
 
-                  <Route exact={true} path="/addVendorProduct/:agentId/:vendorId" element={<AddVendorProducts />} />
-                  <Route exact={true} path="/stocks" element={<UpdateStock />} />
-                  <Route exact={true} path="/transaction" element={<Transaction />} />
-                  <Route exact={true} path="/tax" element={<Taxes />} />
-
-                  <Route exact={true} path="/notifications" element={<Notifications />} />
-                  <Route exact={true} path="/expired" element={<Expiredproducts />} />
-                  <Route exact={true} path="/requests" element={<Requestedorders />} />
+                      <Route exact={true} path="/notifications" element={<Notifications />} />
+                      <Route exact={true} path="/expired" element={<Expiredproducts />} />
+                      <Route exact={true} path="/requests" element={<Requestedorders />} />
+                    </Routes>
+                  )
+                }
 
 
-                </Routes>
+
+
+
+
               </div>
             </main>
           </div>
-        </div>
+        </div >
 
 
 
 
 
-      </div>
+      </div >
     </>
   );
 }
--- src/components/Authentication.js
+++ src/components/Authentication.js
@@ -1,31 +1,45 @@
-import React from 'react'
+import React,{useState} from 'react'
 import '../css/login.css'
 
 const Authentication = () => {
+
+    const[email,setEmail] = useState("")
+    const[password,setPassword] = useState("")
+
+
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+    }
+
   return (
     <>
     
     <div className="login">
         <div className="formss">
           <form noValidate>
-            <span>Login</span>
+            <span>Admin Login</span>
 
             <input
               type="email"
               name="email"
-              placeholder="Enter email id / username"
+              value={email}
+              placeholder="Enter email id"
               className="form-control inp_text"
               id="email"
+              onChange={(e)=>setEmail(e.target.value)}
             />
 
             <input
               type="password"
               name="password"
+              value={password}
               placeholder="Enter password"
               className="form-control"
+              onChange={(e)=>setPassword(e.target.value)}
+
             />
 
-            <button type="submit">Login</button>
+            <button type="submit" onClick={handleSubmit}>Login</button>
           </form>
         </div>
       </div>
